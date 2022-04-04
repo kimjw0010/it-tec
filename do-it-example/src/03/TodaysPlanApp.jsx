122c8b08b5cd3af4d16bc56e9fde03240c7fe736
kimjw0010 <kimjw0012@gmail.com>
2022-04-05 00:25:29 +0900

    04/05/Test

---
+++ do-it-example/src/03/TodaysPlanApp.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TodaysPlan from './TodaysPlan';
+
+class TodaysPlanApp extends Component {
+  render() {
+    return (
+     <div>
+        <TodaysPlan />
+        <TodaysPlan />
+        <TodaysPlan />
+        <TodaysPlan />
+        <TodaysPlan />
+     </div>
+    );
+  }
+}
+
+export default TodaysPlanApp;
